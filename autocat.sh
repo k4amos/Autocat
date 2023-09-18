12c7e0a2dfdee233aa6568545d6d3f13e287f60b
thomas-girard <t.girard912@laposte.net>
2023-09-18 09:27:50 +0200

    wip

--- autocat.sh
+++ autocat.sh
@@ -9,6 +9,7 @@
 
 
 mask_total="?1?2?2?2?2?2?2?3?3?3?3?d?d?d?d"
+
 wordlists=("rockyou.txt")
 
 config_file="config.json"
@@ -22,27 +23,62 @@ OneRuleToRuleThemAll_rules_path=$(jq -r '.OneRuleToRuleThemAll_rules_path' "$con
 
 cracking_sequence="cracking_sequence.txt"
 
-#wordlists_path=$(echo "$wordlists_path" | sed 's/\/$//') # remove potential "/" at the end
+script_args="$@"
+
 run_hashcat() {
-  cat $cracking_sequence | while read line || [ -n "$line" ]
-  do  
 
-    echo $line
-    if [[ $line == *"brute-force"* ]] 
-    then
-        nb_digits=$(echo "$line" | grep -o '[0-9]')
+  echo $script_args
+  regex="/[[:alnum:]_/.-]+" # test if a path is in argument or not
+  if [[ "$script_args" =~ $regex ]]; then
+
+    # cat $cracking_sequence | while read line || [ -n "$line" ]; do 
+    #   echo $line
+    # done
+
+
+    readarray -t lines < $cracking_sequence
+
+    for line in "${lines[@]}"; do
+
+    #cat $cracking_sequence | while read line || [ -n "$line" ]; do 
+
+      echo "$line llllll"
+      if [[ $line == *"brute-force"* ]]; then
+        nb_digits=$(echo "$line" | grep -oE '(0|1?[0-9]|20)')
+        echo "$mask_total $nb_digits"
+        echo "${mask_total:0:($nb_digits)*2}"
         mask="${mask_total:0:($nb_digits)*2}"
+        
 
-        timeout --foreground 3600 hashcat "$@" -a 3 -1 ?l?d?u -2 ?l?d -3 3_default.hcchr $mask -O -w 3 
-    else
+        #timeout --foreground 3600 hashcat $script_args -a 3 -1 ?l?d?u -2 ?l?d -3 3_default.hcchr $mask -O -w 3 
+      else
 
         wordlist=$(echo "$line" | cut -d " " -f 1)
         rule=$(echo "$line" | cut -d " " -f 2)
 
-        timeout --foreground 3600 hashcat "$@" /dico/$wordlist -r /dico/rules/$rule -O -w 3
-        
-    fi
-  done
+        if [ -f "$clem9669_rules_path/$rule" ]; then
+          rule_path=$clem9669_rules_path/$rule
+        elif [ -f "$Hob0Rules_path/$rule" ]; then
+          rule_path=$Hob0Rules_path/$rule
+        elif [ -f "$OneRuleToRuleThemAll_rules_path/$rule" ]; then
+          rule_path=$OneRuleToRuleThemAll_rules_path/$rule
+        elif [ -f "/usr/share/hashcat/rules/$rule" ]; then
+          rule_path="/usr/share/hashcat/rules/$rule"
+        else 
+          echo "$rule still missing :(..."
+          exit 1
+        fi
+        echo $line
+        #hashcat -h
+        #timeout --foreground 3 hashcat -m 1000 ../ntds_test /usr/share/wordlists/wordlists/actors -0
+        timeout --foreground 3600 hashcat $script_args $clem9669_wordlists_path/$wordlist -r $rule_path -O -w 3 
+      
+      fi
+    done
+
+  else
+    hashcat  $script_args
+  fi
 }
 
 download() {
@@ -52,7 +88,8 @@ download() {
     sudo git clone https://github.com/clem9669/wordlists.git $clem9669_wordlists_path
 
     for file in $clem9669_wordlists_path/*.7z; do
-    sudo 7z x "$clem9669_wordlists_path/$file" 2>/dev/null #-o $wordlist_location/wordlists
+      echo "sudo 7z x "$clem9669_wordlists_path/$file" 2>/dev/null #-o $wordlist_location/wordlists"
+      sudo 7z x "$file" 2>/dev/null #-o $wordlist_location/wordlists
     done
 
   fi
@@ -136,8 +173,6 @@ check_for_wordlist() {
     readarray -t lines < $cracking_sequence
 
     for line in "${lines[@]}"; do
-
-      echo "$boolean_ask_for_downloading"
 
       if [[ ! $line = *"brute-force"* ]]; then
 
